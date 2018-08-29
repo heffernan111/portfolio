5c68125f1a9e960d363438eb1fb191af62668203
Unknown <david.heff.dh@googlemail.com>
2018-08-30 00:52:11 +0100

    login drop

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -15,7 +15,9 @@
         <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
         <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
         <!-- Custom styles for this template -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/resume.min.css') }}" rel="stylesheet">
+
         <!-- Bootstrap core JavaScript -->
         <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
         <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
@@ -23,6 +25,7 @@
         <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
         <!-- Custom scripts for this template -->
         <script src="{{ asset('js/resume.js') }}"></script>
+        <script src="{{ asset('js/dropdown.js') }}"></script>
     </head>
     <body>
         <div id="page-top">
@@ -49,11 +52,59 @@
                                 <a class="nav-link js-scroll-trigger" href="{{ url('/home') }}">Home</a>
                             </li>
                         @else
-                            <li class="nav-item">
-                                <a class="nav-link js-scroll-trigger" href="{{ route('login') }}">Login</a>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link js-scroll-trigger" >Login</a>
                             </li>
+                                <div class="dropdown-container">
+                                <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                                        @csrf
+                                        <div class="form-group row">
+                                            <label for="email" class="col-sm-2 col-form-label text-md-right"  ></label>
+                                            <div class="col-md-6">
+                                                <input id="email" placeholder="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                                @if ($errors->has('email'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('email') }}</strong>
+                                                </span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    <div class="form-group row">
+                                        <label for="password" class="col-md-4 col-form-label text-md-right"></label>
+                                        <div class="col-md-6">
+                                            <input id="password" placeholder="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                            @if ($errors->has('password'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-md-6 offset-md-4">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                <label class="form-check-label" for="remember">
+                                                    {{ __('Remember Me') }}
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-0">
+                                        <div class="col-md-8 offset-md-4">
+                                            <button type="submit" class="btn btn-primary">
+                                                {{ __('Login') }}
+                                            </button>
+
+                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                {{ __('Forgot Your Password?') }}
+                                            </a>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                             @endauth
-                        @endif
+                            @endif
                             <li class="nav-item">
                                 <a class="nav-link js-scroll-trigger" href="#">
                                 <i class="fab fa-linkedin-in"></i>
@@ -67,6 +118,23 @@
                         </ul>
                     </div>
             </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             <main class="py-4">
                 @yield('content')
             </main>
